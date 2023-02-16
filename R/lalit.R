7211a3ba2b1e8800902d572a06d3af92c6cc68ea
lalitjoshiii8 <122361193+lalitjoshiii8@users.noreply.github.com>
2023-02-16 10:46:11 +0530

    Add files via upload

---
+++ R/lalit.R
@@ -0,0 +1,25 @@
+#' create histogram , boxplot and numeric summary
+#' @export
+#' @param x numeric variable
+AIML<-function(x){
+
+  #1row 2 column
+
+  par(mflow=c(1,2))
+
+  #histogram
+
+  hist(x,col=rainbow(30))
+
+  #boxplot
+
+  boxplot(x,col="green")
+  par(mflow=c(1,1))
+
+  #numeric summary
+
+  data.frame(min=min(x),
+             median=median(x),
+             max=max(x))
+}
+
